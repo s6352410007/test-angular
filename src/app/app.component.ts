b309330e83930c49a435e779537bf6bd519c80a6
suphakit <s6352410007@sau.ac.th>
2024-07-01 06:16:49 +0700

    test-angular-v1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , NavbarComponent , HomeComponent , AboutMeComponent, FooterComponent , MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'about', component: AboutMeComponent },
+    { path: 'home', component: HomeComponent },
+];
+
+@NgModule ({
+
+    imports: [RouterModule.forChild(routes) ,CommonModule],
+    exports: [RouterModule]
+})
+
+
+export class AppRoutingModule {}
